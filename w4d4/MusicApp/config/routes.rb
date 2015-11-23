dfcc8c0d601fe6dd8a1a5084b61ba1229b0af5ae
Byron Mo <byronmo@gmail.com>
2015-11-22 23:30:25 -0800

    Create Static Pages and updated signin signup

--- w4d4/MusicApp/config/routes.rb
+++ w4d4/MusicApp/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   resources :bands
