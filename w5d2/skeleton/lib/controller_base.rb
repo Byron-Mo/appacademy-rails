275000c1d97646c90601c68c6bdc1f6903f5a161
Byron Mo <byronmo@gmail.com>
2015-11-24 14:28:57 -0800

    Complete Phase 2 ControllerBase

--- w5d2/skeleton/lib/controller_base.rb
+++ w5d2/skeleton/lib/controller_base.rb
@@ -1,6 +1,7 @@
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'active-support/inflector'
 require_relative './session'
 
 class ControllerBase
@@ -8,25 +9,39 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res, route_params = {})
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Already Contains Response" if @already_built_response == true
+    res['Location']=(url)
+    res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Already Contains Response" if @already_built_response == true
+    res['Content-Type'] = content_type
+    res.body = [content]
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    template_file = "views/my_controller/#{template_name.underscore}"
+    contents = File.read(template_file)
   end
 
   # method exposing a `Session` object
@@ -37,4 +52,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
